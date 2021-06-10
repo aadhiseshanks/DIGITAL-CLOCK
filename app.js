972df3c62d81e57c118a911938c1c21b62ac7d0e
aadhiseshanks <83263942+aadhiseshanks@users.noreply.github.com>
2021-06-10 10:39:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+setInterval(function(){
+    const clock = document.querySelector(".display");
+    let time = new Date();
+    let sec = time.getSeconds();
+    let min = time.getMinutes();
+    let hr = time.getHours();
+    let day = 'AM';
+    if(hr >= 12){
+        day = 'PM';
+        hr -= 12;
+    }
+    if(hr == 0){
+        hr = 12;
+    }
+    if(sec < 10){
+        sec = '0' + sec;
+    }
+    if(hr < 10){
+        hr = '0' + hr;
+    }
+    if(min < 10){
+        min = '0' + min;
+    }
+    clock.textContent = hr + ":" + min + ":" + sec + " " + day;
+});
